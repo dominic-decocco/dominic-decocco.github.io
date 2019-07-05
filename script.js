9c8c55f424e26b8abc2ad831df5f6a1c54fde5fb
dominic-decocco <51421614+dominic-decocco@users.noreply.github.com>
2019-07-05 19:07:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var scroll = new SmoothScroll('.navbar a[href*="#"]', {
 	speed: 800
 });
 
+
+
+
+
+
+
+
 // active menu scroll
 $(document).ready(function() {
 		$('a[href*=#]').bind('click', function(e) {
@@ -20,6 +27,12 @@ $(document).ready(function() {
 				return false;
 		});
 });
+
+
+
+
+
+
 
 $(window).scroll(function() {
 		var scrollDistance = $(window).scrollTop();
@@ -39,3 +52,35 @@ $(window).scroll(function() {
 				}
 		});
 }).scroll();
+
+
+
+
+
+
+
+//hamburger menu interaction
+var hamburger = document.getElementById('burger_menu')
+var navbar = document.getElementById('navbar')
+
+hamburger.addEventListener('click', () => {
+	navbar.classList.toggle('open')
+	hamburger.classList.toggle('open')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
